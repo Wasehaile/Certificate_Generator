2f9b1dae148bb0afa6e2bba44600e8c0501cd786
Wasyhun Hailemelekot <wasehaile70@gmail.com>
2023-08-17 03:27:05 -0900

    Setting Up Project

---
+++ src/container/Container.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Input from '../components/Input'
+import Button from '../components/Button'
+
+const Container = () => {
+  return (
+    <div className='w-full h-screen flex items-center gap-5 justify-center flex-col'>
+        <Input/>
+        <Button/>
+    </div>
+  )
+}
+
+export default Container
